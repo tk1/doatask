9203d1a4c3e688d80a7cf84b6d8e3f444207fb36
Tristan Glaes <trglae@gmail.com>
2021-11-22 17:47:01 +0100

    Added test suite for JavaScript code evalutor (made test running)

--- server/src/plugins/code/main.ts
+++ server/src/plugins/code/main.ts
@@ -11,7 +11,7 @@ async function evaluate(submission: Submission, task: Task): Promise<any> {
   //task.details = JSON.parse('{"language":"JavaScript","methodStub":{"functionName":"quadrat","parameter":[{"name":"n","type":"int"}],"returnType":"int"},"testSuite":{"publicTests":[{"testParameter":["0"],"expectedOutput":"0"},{"testParameter":["2"],"expectedOutput":"4"}],"secretTests":[{"testParameter":["-3"],"expectedOutput":"9"}]}}');
   //task.details = JSON.parse('{"language":"JavaScript","methodStub":{"functionName":"power","parameter":[{"name":"base","type":"int"},{"name":"exp","type":"int"}],"returnType":"int"},"testSuite":{"publicTests":[{"testParameter":["0","0"],"expectedOutput":"1"},{"testParameter":["2","2"],"expectedOutput":"4"},{"testParameter":["3","5"],"expectedOutput":"243"}],"secretTests":[{"testParameter":["-3","2"],"expectedOutput":"9"},{"testParameter":["-1","0"],"expectedOutput":"1"}]}}')
   //task.details = JSON.parse('{"language":"Python","methodStub":{"functionName":"sum","parameter":[{"name":"a","type":"int"},{"name":"b","type":"int"}],"returnType":"int"},"testSuite":{"publicTests":[{"testParameter":["0","0"],"expectedOutput":"0"},{"testParameter":["1","1"],"expectedOutput":"2"},{"testParameter":["-2","2"],"expectedOutput":"0"}],"secretTests":[{"testParameter":["-1","-1"],"expectedOutput":"-2"}]}}')
-  task.details = JSON.parse('{"language":"Java","methodStub":{"functionName":"sum","parameter":[{"name":"a","type":"int"},{"name":"b","type":"int"}],"returnType":"int"},"testSuite":{"publicTests":[{"testParameter":["0","0"],"expectedOutput":"0"},{"testParameter":["1","1"],"expectedOutput":"2"},{"testParameter":["-2","2"],"expectedOutput":"0"}],"secretTests":[{"testParameter":["-1","-1"],"expectedOutput":"-2"}]}}')
+  //task.details = JSON.parse('{"language":"Java","methodStub":{"functionName":"sum","parameter":[{"name":"a","type":"int"},{"name":"b","type":"int"}],"returnType":"int"},"testSuite":{"publicTests":[{"testParameter":["0","0"],"expectedOutput":"0"},{"testParameter":["1","1"],"expectedOutput":"2"},{"testParameter":["-2","2"],"expectedOutput":"0"}],"secretTests":[{"testParameter":["-1","-1"],"expectedOutput":"-2"}]}}')
 
   const details:CodeDto = new CodeDto()
   Object.assign(details, task.details)
@@ -19,7 +19,7 @@ async function evaluate(submission: Submission, task: Task): Promise<any> {
   //solution = "function quadrat(n) {return n*n}"
   //solution = "function power(base, exp) { let result = 1; for(let i = 0; i<exp; i++){ result *= base; } return result; }"
   //solution = 'def sum(a, b):\n\treturn a+b'
-  solution = 'int add(int a, int b) { return a+b;}'
+  //solution = 'int add(int a, int b) { return a+b;}'
 
   const codeEvaluator: CodeEvaluator = CodeEvaluatorFactory.createCodeEvaluator(details, solution)
   await codeEvaluator.runAllTests()
