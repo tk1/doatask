c77d5e309ee29b606eb0fc1c0e3ad659bcaaed23
Mohammed Al Ali <mhmmed93m@gmail.com>
2021-12-05 13:28:19 +0100

    Implemented java-script-function-call-builder and refactoring function-call-builder

---
+++ server/src/plugins/code/function-call-builders/function-call-builder-util.ts
@@ -0,0 +1,38 @@
+export class FunctionCallBuilderUtil {
+
+    public buildIntTypeParameter(intParameter: number): string {
+        return intParameter.toString();
+    }
+
+    public buildStringTypeParameterWithSingleQuotes(stringParameter: string): string {
+        return "'" + stringParameter + "'";
+    }
+
+    public buildStringTypeParameterWithDoubleQuotes(stringParameter: string): string {
+        return "\"" + stringParameter + "\"";
+    }
+
+    public buildBooleanTypeParameterWithCapital(booleanParameter: boolean): string {
+        return booleanParameter ? 'True' : 'False';
+    }
+
+    public buildBooleanTypeParameterLowerCase(booleanParameter: boolean): string {
+        return booleanParameter ? 'true' : 'false';
+    }
+
+
+    public buildArrayTypeParameter(arrayParameter: Array<any>, elementBuilder: (element: any) => string): string {
+
+        let result: string = undefined
+
+        for (let currentElementInArray of arrayParameter) {
+            if (result === undefined) {
+                result = '' + elementBuilder(currentElementInArray)
+            } else {
+                result += ', ' + elementBuilder(currentElementInArray)
+            }
+        }
+
+        return '[' + result + ']'
+    }
+}
