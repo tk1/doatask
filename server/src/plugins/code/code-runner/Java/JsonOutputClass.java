ab6992285c5ed96b8aae0ab130884c92221d8eb0
Tristan Glaes <trglae@gmail.com>
2022-02-27 12:35:19 +0100

    Moved JsonOutputClass to own file, added jsonOutputClass.jar, adapted Java Dockerfile and Java code runner

---
+++ server/src/plugins/code/code-runner/Java/JsonOutputClass.java
@@ -0,0 +1,8 @@
+/*
+Compile the class with: javac JsonOutputClass.java
+Build a jar: jar cvf jsonOutputClass.jar JsonOutputClass.class
+ */
+public class JsonOutputClass {
+    public Object returnValue;
+        JsonOutputClass(){}
+}
